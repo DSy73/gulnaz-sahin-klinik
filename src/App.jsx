0bae2a8bc7e7a27fad93b1fada7ab008b4346705
Deniz Sahin Yilmaz <dsylmaz@gmail.com>
2025-12-01 22:10:59 +0300

    Properly rename LoginTemp.jsx to Login.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { supabase } from './supabaseClient';
-import Login from "./LoginTemp";
+import Login from "./Login";
 import {
   Plus,
   Calendar,
